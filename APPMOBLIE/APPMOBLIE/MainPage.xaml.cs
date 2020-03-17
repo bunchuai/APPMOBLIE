836047fc91cc3e0ab74a98f1158e339b239eb7e3
Web Programming Nicky milk <it.pak@hotmail.com>
2020-03-17 09:48:36 +0700

    Add project files.

---
+++ APPMOBLIE/APPMOBLIE/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace APPMOBLIE
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickScan(object sender, EventArgs e)
+        {
+            var Scan = new ZXingScannerPage();
+            Navigation.PushAsync(Scan);
+            Scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    Mycode.Text = result.Text;
+                });
+            };
+        }
+
+        private async void Button_Save(object sender, EventArgs e)
+        {
+            var Models = new Dictionary<string, string>
+            {
+                {"Mycode",this.Mycode.Text},
+                {"ProductName",this.ProductName.Text},
+                {"Location",this.Location.Text},
+                {"Gender",this.Gender.SelectedItem.ToString()},
+                {"Volume",this.Volume.Text}
+            };
+        }
+    }
+}
