09bc2a3bb30970a4333c851ef4b55e092e809e36
Web Programming Nicky milk <it.pak@hotmail.com>
2020-03-17 11:08:06 +0700

    [PAK]  First

---
+++ APPMOBLIE/APPMOBLIE/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APPMOBLIE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_SignIn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Reset(object sender, EventArgs e)
+        {
+            Email.Text = string.Empty;
+            Password.Text = string.Empty;
+        }
+    }
+}
