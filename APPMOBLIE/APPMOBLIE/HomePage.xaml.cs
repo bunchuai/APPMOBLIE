c3a678bdf41cfd8a1514673f0d50826f05a64140
Web Programming Nicky milk <it.pak@hotmail.com>
2020-03-19 16:10:18 +0700

    pak  test connect  to api

--- APPMOBLIE/APPMOBLIE/HomePage.xaml.cs
+++ APPMOBLIE/APPMOBLIE/HomePage.xaml.cs
@@ -9,7 +9,6 @@ using Xamarin.Forms.Xaml;
 
 namespace APPMOBLIE
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : MasterDetailPage
     {
         public HomePage()
