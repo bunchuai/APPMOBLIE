4359d8210752b9760c0b4cb42dfda6efd70fadd8
ทายาท เชื้อเล็ก <thethayart@hotmail.com>
2020-05-26 15:31:15 +0700

    Jean

--- APPMOBLIE/APPMOBLIE/AllTransection.xaml.cs
+++ APPMOBLIE/APPMOBLIE/AllTransection.xaml.cs
@@ -34,26 +34,36 @@ namespace APPMOBLIE
             Username.Text = Application.Current.Properties["Username"].ToString();
             base.OnAppearing();
 
-
-
             using (HttpClient client = new HttpClient())
             {
                 var CompanyId = Application.Current.Properties["CompanyId"];
 
-                string Url = "http://203.151.166.97/api/Products/TransactionBySku?CompanyId=" + CompanyId + "&Take=" + 5 + "&Sku=" + 8857098203187;
+                string Url = "http://203.151.166.97/api/Products/ProductMin?CompanyId=" + CompanyId;
                 client.BaseAddress = new Uri(Url);
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "oLQKZ-tbA58nvbw7PuZhSsy3sr_H28YB3U3XGbpEc5pGIpMrB1nKNjpS_mRhDiFt2QOBZw3IntJ3dmrozZKsw6hd2VuLvoS8-0HxMCVsMUbZ6QZD782Ig1rfFFWPJ13qDq-cMoUgE2t-PdFEp_85aqa8crtVD6aRwntMPjDOgOriFBbzCYjeXyQ3JECl4pOZGd2KYhpCM7n4hXjfCA0t2YeQyvbuId1-e-qhltjEzCkRk7uffgtbwC2KAImsw7jrBYFfxeu1DCRRYdi2AsSZVyBHk0pAqcekzv5jlxLaK2Z-5hFVN0EzSA86Z2MkAq_vXPnJMq0ZrlGfZG6l-hJYb7NjGZCKD44euOf4l-dGQqi40wd8oIhacT1WIrr2RoSAxQn3t1TLDU2TNbgd_pW89JAHd9fmF9k-aZt9tCJuFQs-sW7eJQ1spYqQWHEbKYFbf2Aih5ZBDrIbLeh4hRRFOd_zYZgQKqIZ1tpZ_82UwYUG8FyPn9ZexVzr4t4At4cP");
-                HttpResponseMessage response = await client.GetAsync(Url);
-                if (response.IsSuccessStatusCode)
+                HttpResponseMessage responselow = await client.GetAsync(Url);
+                if (responselow.IsSuccessStatusCode)
                 {
-                    var ResponseData = await response.Content.ReadAsStringAsync();
-                    var TrnsactionResult = JsonConvert.DeserializeObject<List<TransactionModels>>(ResponseData);
-                    Dashboardin.ItemsSource = TrnsactionResult;
-                    Dashboardout.ItemsSource = TrnsactionResult;
+                    //Lows List Product//
+                    var Datalows = await responselow.Content.ReadAsStringAsync();
+                    var TrnsactionResult = JsonConvert.DeserializeObject<List<TransactionLows>>(Datalows);
                     Dashboardreorder.ItemsSource = TrnsactionResult;
+
+                    //In List Product//
+                    HttpResponseMessage responsein = await client.GetAsync("http://203.151.166.97/api/Products/TransactionInProduct?CompanyId=" + CompanyId + "&Take=" + 5);
+                    var Datain = await responsein.Content.ReadAsStringAsync();
+                    var InResult = JsonConvert.DeserializeObject<List<TransactionInOut>>(Datain);
+                    Dashboardin.ItemsSource = InResult;
+
+                    //Out List Product//
+                    HttpResponseMessage responseout = await client.GetAsync("http://203.151.166.97/api/Products/TransactionOutProduct?CompanyId=" + CompanyId + "&Take=" + 5);
+                    var Dataout = await responseout.Content.ReadAsStringAsync();
+                    var OutResult = JsonConvert.DeserializeObject<List<TransactionInOut>>(Dataout);
+                    Dashboardout.ItemsSource = OutResult;
                 }
+
             }
         }
 
---
+++ APPMOBLIE/APPMOBLIE/Model/TransactionLows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APPMOBLIE.Model
+{
+        public class TransactionLows
+        {
+        public string SkuId { get; set; }
+        public string ProductName { get; set; }
+        public string Status { get; set; }
+        public string Total { get; set; }
+        }
+
+    public class TransactionInOut
+    {
+        public string Ref { get; set; }
+        public string CreateDate { get; set; }
+        public string Amount { get; set; }
+        public string Locations { get; set; }
+        public string Username { get; set; }
+    }
+}
