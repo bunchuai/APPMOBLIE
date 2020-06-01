b0687c9ff8cad905ccc70d78c0406096a5fc7c26
YoYoTNS <YoYoTNS@AIO-WIMON>
2020-06-01 13:16:50 +0700

    KIK 13.16  forgotpassword

---
+++ APPMOBLIE/APPMOBLIE/Forgotpassword.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APPMOBLIE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Forgotpassword : ContentPage
+    {
+        public Forgotpassword()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
