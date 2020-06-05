826d02e5dcc42d59ab484c8a710245702e11ce40
Web Programming Nicky milk <it.pak@hotmail.com>
2020-06-05 14:33:43 +0700

    pak validate add product

--- APPMOBLIE/APPMOBLIE/Behavior/EmailValidation.cs
+++ APPMOBLIE/APPMOBLIE/Behavior/EmailValidation.cs
@@ -136,6 +136,43 @@ namespace APPMOBLIE.Validations
         }
     }
 
+    class NumberOnlyValidation2 : Behavior<Entry>
+    {
+        public static readonly BindablePropertyKey bindablePropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(NumberOnlyValidation2), false);
+        public static readonly BindableProperty bindableProperty = bindablePropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)this.GetValue(bindableProperty);
+            }
+            set
+            {
+                this.SetValue(bindablePropertyKey, value);
+            }
+        }
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += HandlerTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= HandlerTextChanged;
+        }
+
+        void HandlerTextChanged(Object sender, TextChangedEventArgs e)
+        {
+            Entry entry;
+            entry = (Entry)sender;
+            this.IsValid = (Regex.IsMatch(e.NewTextValue, @"[\d]{1,4}([.,][\d]{1,2})?"));
+            entry.TextColor = this.IsValid ? Color.Default : Color.Red;
+        }
+    }
+
     class EmptyValidation : Behavior<Entry>
     {
         public static readonly BindablePropertyKey bindablePropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(EmptyValidation), false);
@@ -188,4 +225,213 @@ namespace APPMOBLIE.Validations
             entry.TextColor = this.IsValid ? Color.Default : Color.Red;
         }
     }
+    class EmptyValidation2 : Behavior<Entry>
+    {
+        public static readonly BindablePropertyKey bindablePropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(EmptyValidation2), false);
+        public static readonly BindableProperty bindableProperty = bindablePropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)GetValue(bindableProperty);
+            }
+            set
+            {
+                this.SetValue(bindablePropertyKey, value);
+            }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += HandlerTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= HandlerTextChanged;
+        }
+
+        void HandlerTextChanged(Object sender, TextChangedEventArgs e)
+        {
+            Entry entry;
+            entry = (Entry)sender;
+            if (e.NewTextValue != null)
+            {
+                if (e.NewTextValue.Length == 0)
+                {
+                    this.IsValid = false;
+                }
+                else
+                {
+                    this.IsValid = true;
+                }
+            }
+            else
+            {
+                this.IsValid = false;
+            }
+
+            entry.TextColor = this.IsValid ? Color.Default : Color.Red;
+        }
+    }
+    class EmptyValidation3 : Behavior<Entry>
+    {
+        public static readonly BindablePropertyKey bindablePropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(EmptyValidation3), false);
+        public static readonly BindableProperty bindableProperty = bindablePropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)GetValue(bindableProperty);
+            }
+            set
+            {
+                this.SetValue(bindablePropertyKey, value);
+            }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += HandlerTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= HandlerTextChanged;
+        }
+
+        void HandlerTextChanged(Object sender, TextChangedEventArgs e)
+        {
+            Entry entry;
+            entry = (Entry)sender;
+            if (e.NewTextValue != null)
+            {
+                if (e.NewTextValue.Length == 0)
+                {
+                    this.IsValid = false;
+                }
+                else
+                {
+                    this.IsValid = true;
+                }
+            }
+            else
+            {
+                this.IsValid = false;
+            }
+
+            entry.TextColor = this.IsValid ? Color.Default : Color.Red;
+        }
+    }
+    class EmptyValidation4 : Behavior<Entry>
+    {
+        public static readonly BindablePropertyKey bindablePropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(EmptyValidation4), false);
+        public static readonly BindableProperty bindableProperty = bindablePropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)GetValue(bindableProperty);
+            }
+            set
+            {
+                this.SetValue(bindablePropertyKey, value);
+            }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += HandlerTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= HandlerTextChanged;
+        }
+
+        void HandlerTextChanged(Object sender, TextChangedEventArgs e)
+        {
+            Entry entry;
+            entry = (Entry)sender;
+            if (e.NewTextValue != null)
+            {
+                if (e.NewTextValue.Length == 0)
+                {
+                    this.IsValid = false;
+                }
+                else
+                {
+                    this.IsValid = true;
+                }
+            }
+            else
+            {
+                this.IsValid = false;
+            }
+
+            entry.TextColor = this.IsValid ? Color.Default : Color.Red;
+        }
+    }
+    class EmptyValidation5 : Behavior<Entry>
+    {
+        public static readonly BindablePropertyKey bindablePropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(EmptyValidation5), false);
+        public static readonly BindableProperty bindableProperty = bindablePropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)GetValue(bindableProperty);
+            }
+            set
+            {
+                this.SetValue(bindablePropertyKey, value);
+            }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += HandlerTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= HandlerTextChanged;
+        }
+
+        void HandlerTextChanged(Object sender, TextChangedEventArgs e)
+        {
+            Entry entry;
+            entry = (Entry)sender;
+            if (e.NewTextValue != null)
+            {
+                if (e.NewTextValue.Length == 0)
+                {
+                    this.IsValid = false;
+                }
+                else
+                {
+                    this.IsValid = true;
+                }
+            }
+            else
+            {
+                this.IsValid = false;
+            }
+
+            entry.TextColor = this.IsValid ? Color.Default : Color.Red;
+        }
+    }
+
 }
