9bae928e834b32373e8ca00a7bdd0fcb5bee904c
ทายาท เชื้อเล็ก <thethayart@hotmail.com>
2020-06-24 16:14:22 +0700

    JEAN PulltoRefresh

--- APPMOBLIE/APPMOBLIE/AllTransection.xaml.cs
+++ APPMOBLIE/APPMOBLIE/AllTransection.xaml.cs
@@ -20,51 +20,54 @@ namespace APPMOBLIE
         public bool dashboardIn { get; set; }
         public bool dashboardOut { get; set; }
         public bool dashboardRe { get; set; }
+        public List<TransactionInOut> InResultData = new List<TransactionInOut>();
+        public List<TransactionInOut> OutResultData = new List<TransactionInOut>();
+        public List<TransactionInOut> LowResultData = new List<TransactionInOut>();
 
-        //List<Entry> entries = new List<Entry>
-        //{
-        //    new Entry(100)
-        //    {
-        //        Color=SKColor.Parse("#FF1943"),
-        //        Label ="PRODUCT A",
-        //        ValueLabel = "100"
-        //    },
-        //    new Entry(200)
-        //    {
-        //        Color = SKColor.Parse("00BFFF"),
-        //        Label = "PRODUCT B",
-        //        ValueLabel = "200"
-        //    },
-        //    new Entry(150)
-        //    {
-        //        Color =  SKColor.Parse("#00CED1"),
-        //        Label = "PRODUCT C",
-        //        ValueLabel = "150"
-        //    },
-        // };
-
-        public AllTransection()
+        async Task<List<TransactionInOut>> GetDataInList()
         {
-            InitializeComponent();
-            dashboardIn = false;
-            dashboardOut = false;
-            dashboardRe = false;
-            Dashboardin.IsVisible = dashboardIn;
-            Dashboardout.IsVisible = dashboardOut;
-            Dashboardreorder.IsVisible = dashboardRe;
+            using (HttpClient client = new HttpClient())
+            {
+                var CompanyId = Application.Current.Properties["CompanyId"];
 
-            //Chart1.Chart = new LineChart() 
-            //{ 
-            //    Entries = entries
-            //};
+                string Url = "http://203.151.166.97/api/Products/TransactionInProduct?CompanyId=" + CompanyId + "&Take=" + 5;
+                client.BaseAddress = new Uri(Url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "oLQKZ-tbA58nvbw7PuZhSsy3sr_H28YB3U3XGbpEc5pGIpMrB1nKNjpS_mRhDiFt2QOBZw3IntJ3dmrozZKsw6hd2VuLvoS8-0HxMCVsMUbZ6QZD782Ig1rfFFWPJ13qDq-cMoUgE2t-PdFEp_85aqa8crtVD6aRwntMPjDOgOriFBbzCYjeXyQ3JECl4pOZGd2KYhpCM7n4hXjfCA0t2YeQyvbuId1-e-qhltjEzCkRk7uffgtbwC2KAImsw7jrBYFfxeu1DCRRYdi2AsSZVyBHk0pAqcekzv5jlxLaK2Z-5hFVN0EzSA86Z2MkAq_vXPnJMq0ZrlGfZG6l-hJYb7NjGZCKD44euOf4l-dGQqi40wd8oIhacT1WIrr2RoSAxQn3t1TLDU2TNbgd_pW89JAHd9fmF9k-aZt9tCJuFQs-sW7eJQ1spYqQWHEbKYFbf2Aih5ZBDrIbLeh4hRRFOd_zYZgQKqIZ1tpZ_82UwYUG8FyPn9ZexVzr4t4At4cP");
+                HttpResponseMessage responsedatain = await client.GetAsync(Url);
+                if (responsedatain.IsSuccessStatusCode)
+                {
+                    //In List Product//
+                    var Datain = await responsedatain.Content.ReadAsStringAsync();
+                    InResultData = JsonConvert.DeserializeObject<List<TransactionInOut>>(Datain);
+                }
+            }
+            return InResultData;
         }
-
-
-        protected override async void OnAppearing()
+        async Task<List<TransactionInOut>> GetDataOutList()
         {
-            Username.Text = Application.Current.Properties["Username"].ToString();
-            base.OnAppearing();
+            using (HttpClient client = new HttpClient())
+            {
+                var CompanyId = Application.Current.Properties["CompanyId"];
 
+                string Url = "http://203.151.166.97/api/Products/TransactionOutProduct?CompanyId=" + CompanyId + "&Take=" + 5;
+                client.BaseAddress = new Uri(Url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "oLQKZ-tbA58nvbw7PuZhSsy3sr_H28YB3U3XGbpEc5pGIpMrB1nKNjpS_mRhDiFt2QOBZw3IntJ3dmrozZKsw6hd2VuLvoS8-0HxMCVsMUbZ6QZD782Ig1rfFFWPJ13qDq-cMoUgE2t-PdFEp_85aqa8crtVD6aRwntMPjDOgOriFBbzCYjeXyQ3JECl4pOZGd2KYhpCM7n4hXjfCA0t2YeQyvbuId1-e-qhltjEzCkRk7uffgtbwC2KAImsw7jrBYFfxeu1DCRRYdi2AsSZVyBHk0pAqcekzv5jlxLaK2Z-5hFVN0EzSA86Z2MkAq_vXPnJMq0ZrlGfZG6l-hJYb7NjGZCKD44euOf4l-dGQqi40wd8oIhacT1WIrr2RoSAxQn3t1TLDU2TNbgd_pW89JAHd9fmF9k-aZt9tCJuFQs-sW7eJQ1spYqQWHEbKYFbf2Aih5ZBDrIbLeh4hRRFOd_zYZgQKqIZ1tpZ_82UwYUG8FyPn9ZexVzr4t4At4cP");
+                HttpResponseMessage responsedataout = await client.GetAsync(Url);
+                if (responsedataout.IsSuccessStatusCode)
+                {
+                    //In List Product//
+                    var Dataout = await responsedataout.Content.ReadAsStringAsync();
+                    OutResultData = JsonConvert.DeserializeObject<List<TransactionInOut>>(Dataout);
+                }
+            }
+            return OutResultData;
+        }
+        async Task<List<TransactionInOut>> GetDataLowList()
+        {
             using (HttpClient client = new HttpClient())
             {
                 var CompanyId = Application.Current.Properties["CompanyId"];
@@ -74,28 +77,36 @@ namespace APPMOBLIE
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "oLQKZ-tbA58nvbw7PuZhSsy3sr_H28YB3U3XGbpEc5pGIpMrB1nKNjpS_mRhDiFt2QOBZw3IntJ3dmrozZKsw6hd2VuLvoS8-0HxMCVsMUbZ6QZD782Ig1rfFFWPJ13qDq-cMoUgE2t-PdFEp_85aqa8crtVD6aRwntMPjDOgOriFBbzCYjeXyQ3JECl4pOZGd2KYhpCM7n4hXjfCA0t2YeQyvbuId1-e-qhltjEzCkRk7uffgtbwC2KAImsw7jrBYFfxeu1DCRRYdi2AsSZVyBHk0pAqcekzv5jlxLaK2Z-5hFVN0EzSA86Z2MkAq_vXPnJMq0ZrlGfZG6l-hJYb7NjGZCKD44euOf4l-dGQqi40wd8oIhacT1WIrr2RoSAxQn3t1TLDU2TNbgd_pW89JAHd9fmF9k-aZt9tCJuFQs-sW7eJQ1spYqQWHEbKYFbf2Aih5ZBDrIbLeh4hRRFOd_zYZgQKqIZ1tpZ_82UwYUG8FyPn9ZexVzr4t4At4cP");
-                HttpResponseMessage responselow = await client.GetAsync(Url);
-                if (responselow.IsSuccessStatusCode)
+                HttpResponseMessage responsedatalow = await client.GetAsync(Url);
+                if (responsedatalow.IsSuccessStatusCode)
                 {
-                    //Lows List Product//
-                    var Datalows = await responselow.Content.ReadAsStringAsync();
-                    var TrnsactionResult = JsonConvert.DeserializeObject<List<TransactionLows>>(Datalows);
-                    Dashboardreorder.ItemsSource = TrnsactionResult;
-
                     //In List Product//
-                    HttpResponseMessage responsein = await client.GetAsync("http://203.151.166.97/api/Products/TransactionInProduct?CompanyId=" + CompanyId + "&Take=" + 5);
-                    var Datain = await responsein.Content.ReadAsStringAsync();
-                    var InResult = JsonConvert.DeserializeObject<List<TransactionInOut>>(Datain);
-                    Dashboardin.ItemsSource = InResult;
-
-                    //Out List Product//
-                    HttpResponseMessage responseout = await client.GetAsync("http://203.151.166.97/api/Products/TransactionOutProduct?CompanyId=" + CompanyId + "&Take=" + 5);
-                    var Dataout = await responseout.Content.ReadAsStringAsync();
-                    var OutResult = JsonConvert.DeserializeObject<List<TransactionInOut>>(Dataout);
-                    Dashboardout.ItemsSource = OutResult;
+                    var Datalow = await responsedatalow.Content.ReadAsStringAsync();
+                    LowResultData = JsonConvert.DeserializeObject<List<TransactionInOut>>(Datalow);
                 }
-
             }
+            return LowResultData;
+        }
+        public AllTransection()
+        {
+            InitializeComponent();
+            dashboardIn = false;
+            dashboardOut = false;
+            dashboardRe = false;
+            Dashboardin.IsVisible = dashboardIn;
+            Dashboardout.IsVisible = dashboardOut;
+            Dashboardreorder.IsVisible = dashboardRe;
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            Username.Text = Application.Current.Properties["Username"].ToString();
+            base.OnAppearing();
+
+            Dashboardin.ItemsSource = await GetDataInList();
+            Dashboardout.ItemsSource = await GetDataOutList();
+            Dashboardreorder.ItemsSource = await GetDataLowList();
         }
 
         public void Button_ClickedIn(object sender, EventArgs e)
@@ -128,7 +139,6 @@ namespace APPMOBLIE
             }
 
         }
-
         public void Button_ClickedReorder(object sender, EventArgs e)
         {
 
@@ -144,12 +154,26 @@ namespace APPMOBLIE
             }
 
         }
+        private async void Dashboardin_Refreshing(object sender, EventArgs e)
+        {
+            Dashboardin.ItemsSource = await GetDataInList();
+            Dashboardin.EndRefresh();
+        }
+        private async void Dashboardout_Refreshing(object sender, EventArgs e)
+        {
+            Dashboardout.ItemsSource = await GetDataOutList();
+            Dashboardout.EndRefresh();
+        }
+        private async void Dashboardreorder_Refreshing(object sender, EventArgs e)
+        {
+            Dashboardreorder.ItemsSource = await GetDataLowList();
+            Dashboardreorder.EndRefresh();
+        }
 
         private void BtnStockin(object sender, EventArgs e)
         {
             this.Navigation.PushAsync(new StockIn());
         }
-
         private void BtnStockOut(object sender, EventArgs e)
         {
             this.Navigation.PushAsync(new StockOut());
