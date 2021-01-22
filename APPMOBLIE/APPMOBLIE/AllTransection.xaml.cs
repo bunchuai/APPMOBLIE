33aa518ed5699dc885cc1d76f9a9094a702043e5
Tns_soft <Tns_soft@DESKTOP-JAQ9PJA>
2021-01-22 16:16:32 +0700

    KIK 22-1-2021 16.16

--- APPMOBLIE/APPMOBLIE/AllTransection.xaml.cs
+++ APPMOBLIE/APPMOBLIE/AllTransection.xaml.cs
@@ -12,6 +12,7 @@ namespace APPMOBLIE
 {
     public partial class AllTransection : ContentPage 
     {
+
        
         //public bool DashboardIn { get; set; }
         //public bool DashboardOut { get; set; }
@@ -101,6 +102,7 @@ namespace APPMOBLIE
         }
 
 
+
         protected override async void OnAppearing()
         {
             var CompanyId = Application.Current.Properties["CompanyId"];
@@ -131,13 +133,35 @@ namespace APPMOBLIE
 
             }
 
-            DateTimeNow.Text = DateTime.Now.ToString("MM/dd/yyyy"); 
+
+            using (HttpClient client = new HttpClient())
+            {
+               
+
+                string Url = "http://203.151.166.97/api/Products/TransactionInProduct?CompanyId=" + CompanyId + "&Take=" + 15;
+                client.BaseAddress = new Uri(Url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "oLQKZ-tbA58nvbw7PuZhSsy3sr_H28YB3U3XGbpEc5pGIpMrB1nKNjpS_mRhDiFt2QOBZw3IntJ3dmrozZKsw6hd2VuLvoS8-0HxMCVsMUbZ6QZD782Ig1rfFFWPJ13qDq-cMoUgE2t-PdFEp_85aqa8crtVD6aRwntMPjDOgOriFBbzCYjeXyQ3JECl4pOZGd2KYhpCM7n4hXjfCA0t2YeQyvbuId1-e-qhltjEzCkRk7uffgtbwC2KAImsw7jrBYFfxeu1DCRRYdi2AsSZVyBHk0pAqcekzv5jlxLaK2Z-5hFVN0EzSA86Z2MkAq_vXPnJMq0ZrlGfZG6l-hJYb7NjGZCKD44euOf4l-dGQqi40wd8oIhacT1WIrr2RoSAxQn3t1TLDU2TNbgd_pW89JAHd9fmF9k-aZt9tCJuFQs-sW7eJQ1spYqQWHEbKYFbf2Aih5ZBDrIbLeh4hRRFOd_zYZgQKqIZ1tpZ_82UwYUG8FyPn9ZexVzr4t4At4cP");
+                HttpResponseMessage responsedatain = await client.GetAsync(Url);
+                if (responsedatain.IsSuccessStatusCode)
+                {
+                    var ReponseData = await responsedatain.Content.ReadAsStringAsync();
+                    var Result = JsonConvert.DeserializeObject<List<TransactionInOut>>(ReponseData);
+                    ListTranIn.ItemsSource = Result;
+                }
+            }
+
+
+            DateTimeNow.Text = DateTime.Now.ToString("dd/MM/yyyy"); 
 
             //Dashboardin.ItemsSource = await GetDataInList();
             //Dashboardout.ItemsSource = await GetDataOutList();
             //Dashboardreorder.ItemsSource = await GetDataLowList();
 
             base.OnAppearing();
+
+
 
         }
         public async void GetDataUser()
@@ -159,6 +183,8 @@ namespace APPMOBLIE
                 }
 
             }
+
+
         }
         public void Button_ClickedIn(object sender, EventArgs e)
         {
@@ -234,6 +260,11 @@ namespace APPMOBLIE
         private void ImageButton_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new EditUser()); 
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new Transactions());
         }
 
         //private void btnTranIn(object sender, EventArgs e)
